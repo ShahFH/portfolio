a8285526699e3dc50361df88270c3f3cd04a8fe8
ShahFH <shahfaisal.swe@gmail.com>
2024-04-25 15:14:43 +0430

    "Add Cursor component to home page"
    This commit adds a new component, `Cursor`, to the home page. The `Cursor` component is imported at the top of the file and then rendered within the `Home` component.

--- app/page.js
+++ app/page.js
@@ -6,10 +6,12 @@ import Navbar from "./components/navbar";
 import Project from './components/projects'
 import Contact from './components/contact'
 import Footer from './components/footer'
+import Cursor from './anim/Cursor'
 
 export default function Home() {
   return (
     <>
+    <Cursor />
     <Navbar />
     <MainPage />
     <About />
