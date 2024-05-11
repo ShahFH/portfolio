908f78f0835e41c487b1093298ff50a60507e28c
ShahFH <shahfaisal.swe@gmail.com>
2024-05-11 14:14:14 +0430

    init

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -22,7 +22,7 @@ export default function Projects() {
         <div className="content-box flex flex-col justify-start md:justify-end p-8">
           <TextReveal className=" text-3xl tracking-tight font-[Work Sans] mb-4" title="Blog Website" />
             
-          <p className="">
+          <p className=" text-[#9b9b9b]">
             Web Development, Astro, React, Tailwind
           </p>
         </div>
@@ -32,8 +32,8 @@ export default function Projects() {
     <div className="grid md:grid-cols-2 pt-15">
         {/** Side 2: Title and Description */}
         <div className="content-box flex flex-col justify-end items-start md:items-end p-8 order-2 md:order-1">
-        <TextReveal className=" text-3xl tracking-tight font-[Work Sans] mb-4" title="BGStrip Web App" />
-          <p className="">
+        <TextReveal className=" text-3xl tracking-tight font-[Work Sans] mb-4" title="BG-Strip Web App" />
+        <p className=" text-[#9b9b9b]">
             Web Design & Development, Framer-motion, React, Tailwind
           </p>
         </div>
@@ -57,7 +57,7 @@ export default function Projects() {
         {/** Side 2: Title and Description */}
         <div className="content-box flex flex-col justify-start md:justify-end p-8">
         <TextReveal className=" text-3xl tracking-tight font-[Work Sans] mb-4" title="Xplore Travel Companion" />
-          <p className="">
+          <p className=" text-[#9b9b9b]">
             Web Development, React, Web Design
           </p>
         </div>
@@ -68,7 +68,7 @@ export default function Projects() {
         {/** Side 2: Title and Description */}
         <div className="content-box flex flex-col justify-end items-start md:items-end p-8 order-2 md:order-1">
         <TextReveal className=" text-3xl tracking-tight font-[Work Sans] mb-4" title="Portfolio" />
-          <p className="">
+          <p className=" text-[#9b9b9b]">
             Web Design & Development, Framer-motion, React, Tailwind
           </p>
         </div>
--- app/components/service.jsx
+++ app/components/service.jsx
@@ -5,7 +5,7 @@ import TextReveal from '../anim/TextReveal'
 export default function service() {
   return (
     <div data-scroll-container id='services' className="">
-        <section data-scroll-section className="smooth-content">
+        <section data-scroll-section data-scroll  data-scroll-speed=".2" className="smooth-content">
           <div className=' grid md:grid-cols-2 mx-auto justify-center px-10 md:px-52'>
             <h1 className='text-start text-[8vw] tracking-tighter font-semibold'>Ŵeb Đevelopment</h1>
             <p className='text-base font-medium text-secondary-50 opacity-75'>I specialize in crafting custom websites that reflect your brand, focusing on scalability, performance, accessibility, and engaging animations. 
@@ -13,7 +13,7 @@ export default function service() {
           </div>
         </section>
 
-        <section data-scroll-section className="smooth-content">
+        <section data-scroll-section data-scroll  data-scroll-speed=".2" className="smooth-content">
         <div className=' grid md:grid-cols-2 mx-auto justify-center px-10 md:px-52'>
             <h1 className='text-start text-[8vw] tracking-tighter font-semibold'>Ŵeb Design</h1>
             <p className='text-base font-medium text-secondary-50 opacity-75'>As a creative visionary in the digital realm, I specialize in web design, where I blend artistry with functionality to craft captivating online experiences. 
