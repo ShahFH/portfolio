825f55415596edb874a82220799b6f4b30714921
ShahFH <shahfaisal.swe@gmail.com>
2024-06-07 14:38:59 +0430

    final int

--- app/page.jsx
+++ app/page.jsx
@@ -7,10 +7,9 @@ import Project from './components/projects'
 import Contact from './components/contact'
 import Footer from './components/footer'
 import Cursor from './anim/Cursor'
-import LocomotiveScroll from 'locomotive-scroll';
 
 export default function Home() {
-  const locomotiveScroll = new LocomotiveScroll();
+
   return (
     <>
     <Cursor />
